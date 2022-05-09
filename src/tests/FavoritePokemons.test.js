75332d374f577b3872c59d17f2907db8fa3f987d
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-05-09 19:58:49 -0300

    adiciona requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -4,14 +4,14 @@ import RenderWithRouter from '../services/RenderWithRouter';
 
 describe('Testa se <FavoritePokemons.js /> tem o comportamento sperado', () => {
   it('Testa se a página exibe o conteudo correto', () => {
-    const { queryAllByRole, getByText, queryByRole } = RenderWithRouter(<FavoritePokemons />);
-    const favorites = queryAllByRole('dialog')
+    const { queryAllByRole, getByText } = RenderWithRouter(<FavoritePokemons />);
+    const favorites = queryAllByRole('dialog');
     const noFavorites = getByText(/No favorite pokemon found/i);
 
     if (favorites.length === 0) {
       expect(noFavorites).toBeInTheDocument();
     }
-    
+
     if (favorites.length > 0) {
       favorites.forEach((pok) => expect(pok).toBeInTheDocument());
       expect(noFavorites).not.toBeInTheDocument();
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -2,4 +2,11 @@ import React from 'react';
 import NotFound from '../components/NotFound';
 import RenderWithRouter from '../services/RenderWithRouter';
 
-test('', () => {});
+it('Testa o conteudo da página <NotFound.js />', () => {
+  const { getByRole, getByAltText } = RenderWithRouter(<NotFound />);
+  const text = getByRole('heading', { level: 2, name: /Page requested not found/i });
+  const image = getByAltText(/Pikachu crying because the page/i);
+
+  expect(text).toBeInTheDocument();
+  expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
