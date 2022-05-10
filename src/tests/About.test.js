2cb5caeb13d315bf8b01f870699928d0fcde5f2a
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-05-10 00:32:12 -0300

    adiciona parte do requisito 5

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 // import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+// import userEvent from '@testing-library/user-event';
 import RenderWithRouter from '../services/RenderWithRouter';
 import About from '../components/About';
 
 describe('Teste se o componente <About /> tem o comportamento esperado', () => {
   it('Testa se a página contém as informações sobre a Pokédex', () => {
-    const { getByText, history, getByRole, getByAltText } = RenderWithRouter(<About />);
+    const { getByText, getByRole, getByAltText } = RenderWithRouter(<About />);
     const about = getByText(/This application simulates a Pokédex/i);
     const heading = getByRole('heading', { level: 2, name: /About Pokédex/i });
     const image = getByAltText('Pokédex');
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,82 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import RenderWithRouter from '../services/RenderWithRouter';
+import pokemons from '../data';
+
+describe('Testa o componente <Pokedex.js />', () => {
+  it('Testa se a página contém um heading h2 com o texto Encountered pokémons', () => {
+    const { getByRole } = RenderWithRouter(<App />);
+    const heading = getByRole('heading', { level: 2, name: /Encountered pokémons/i });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Testa se o botão "Próximo pokémon" tem o comportamento esperado', () => {
+    const { getByTestId, getByText, getAllByTestId } = RenderWithRouter(<App />);
+
+    const proximoPokemon = getByTestId('next-pokemon');
+    const pokemonCard = getByTestId('pokemon-name');
+    const allPokemonCard = getAllByTestId('pokemon-name');
+
+    expect(allPokemonCard).toHaveLength(1);
+    expect(proximoPokemon).toBeInTheDocument();
+    expect(proximoPokemon).toHaveTextContent('Próximo pokémon');
+    expect(pokemonCard).toBeInTheDocument();
+
+    pokemons.forEach(({ name }) => {
+      const displayedPokemon = getByText(name);
+      expect(displayedPokemon).toBeInTheDocument();
+      userEvent.click(proximoPokemon);
+    });
+
+    const displayedPokemon = getByText(/pikachu/i);
+    expect(displayedPokemon).toBeInTheDocument();
+  });
+
+  it('Testa se a Pokédex tem os botões de filtro.', () => {
+    const { getByText, getAllByTestId,
+      getByRole } = RenderWithRouter(<App />);
+
+    const seven = 7;
+    const typeButton = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic',
+      'Normal', 'Dragon'];
+    const buttons = getAllByTestId('pokemon-type-button');
+    const buttonAll = getByRole('button', { name: 'All' });
+    const buttonNext = getByRole('button', { name: /Próximo pokémon/i });
+
+    buttons.forEach((button, index) => {
+      expect(button).toBeInTheDocument();
+      expect(button).toHaveTextContent(typeButton[index]);
+      expect(buttonAll).toBeInTheDocument();
+      userEvent.click(button);
+
+      pokemons.filter((pok) => pok.type === typeButton[index])
+        .forEach((p, i) => {
+          if (i > 1) {
+            userEvent.click(buttonNext);
+            const pokeType = getByText(p.name);
+            expect(pokeType).toBeInTheDocument();
+            expect(buttonAll).toBeInTheDocument();
+          }
+          if (i === 0) {
+            const pokeType = getByText(p.name);
+            expect(pokeType).toBeInTheDocument();
+            expect(buttonAll).toBeInTheDocument();
+          }
+        });
+    });
+
+    expect(buttons).toHaveLength(seven);
+    expect(buttonAll).toBeInTheDocument();
+  });
+
+  it('Testa se a Pokédex contém um botão para resetar o filtro', () => {
+    const { getByRole } = RenderWithRouter(<App />);
+
+    const buttonAll = getByRole('button', { name: 'All' });
+    // userEvent.click(buttonAll);
+
+    console.log('=======', buttonAll);
+  });
+});
