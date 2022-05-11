d7f36fc9a649321e6890c1c9f7f955526e458253
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-05-10 21:00:07 -0300

    adiciona correção de pagina do requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -4,31 +4,29 @@ import App from '../App';
 import RenderWithRouter from './RenderWithRouter';
 
 describe('Testa o componente <Pokemon.js />', () => {
-  // beforeEach(() => RenderWithRouter(<App />));
   it('Testa se é renderizado um card com as informações do pokémon', () => {
-    const { history, getByRole, getByTestId } = RenderWithRouter(<App />);
+    const { getByRole, getByTestId } = RenderWithRouter(<App />);
 
     const pokeInfo = getByRole('link', { name: /More details/i });
     expect(pokeInfo).toBeInTheDocument();
-    userEvent.click(pokeInfo);
+    // userEvent.click(pokeInfo);
 
-    expect(getByRole('heading', {
-      level: 2, name: 'Pikachu Details' })).toBeInTheDocument();
     expect(getByTestId('pokemon-name')).toHaveTextContent(/Pikachu/i);
     expect(getByTestId('pokemon-type')).toHaveTextContent(/Electric/i);
     expect(getByTestId('pokemon-weight')).toHaveTextContent(
       /Average weight: 6.0 kg/i,
     );
-
-    const { pathname } = history.location;
-    expect(pathname).toBe('/pokemons/25');
   });
 
   it('Testa se o pokemon é exibido com as imagens corretas', () => {
-    const { getByRole, getByLabelText } = RenderWithRouter(<App />);
+    const { getByRole, getByLabelText, history } = RenderWithRouter(<App />);
 
     userEvent.click(getByRole('link', { name: /More details/i }));
     userEvent.click(getByLabelText(/Pokémon favoritado?/i));
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+    userEvent.click(getByRole('link', { name: /home/i }));
 
     const pokImage = getByRole('img', { name: 'Pikachu sprite' });
     expect(pokImage).toBeInTheDocument();
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+// import userEvent from '@testing-library/user-event';
+// import React from 'react';
+// import App from '../App';
+// import RenderWithRouter from './RenderWithRouter';
+
+// describe('Testa o componente <Pokemon.js />', () => {
+//   // beforeEach(() => RenderWithRouter(<App />));
+//   it('Testa se é renderizado um card com as informações do pokémon', () => {
+//     const { history, getByRole, getByTestId } = RenderWithRouter(<App />);
+
+//     const pokeInfo = getByRole('link', { name: /More details/i });
+//     expect(pokeInfo).toBeInTheDocument();
+//     userEvent.click(pokeInfo);
+
+//     expect(getByRole('heading', {
+//       level: 2, name: 'Pikachu Details' })).toBeInTheDocument();
+//     expect(getByTestId('pokemon-name')).toHaveTextContent(/Pikachu/i);
+//     expect(getByTestId('pokemon-type')).toHaveTextContent(/Electric/i);
+//     expect(getByTestId('pokemon-weight')).toHaveTextContent(
+//       /Average weight: 6.0 kg/i,
+//     );
+
+//     const { pathname } = history.location;
+//     expect(pathname).toBe('/pokemons/25');
+//   });
+
+//   it('Testa se o pokemon é exibido com as imagens corretas', () => {
+//     const { getByRole, getByLabelText } = RenderWithRouter(<App />);
+
+//     userEvent.click(getByRole('link', { name: /More details/i }));
+//     userEvent.click(getByLabelText(/Pokémon favoritado?/i));
+
+//     const pokImage = getByRole('img', { name: 'Pikachu sprite' });
+//     expect(pokImage).toBeInTheDocument();
+//     expect(pokImage.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+
+//     const starImage = getByRole('img', { name: 'Pikachu is marked as favorite' });
+//     expect(starImage.src).toBe('http://localhost/star-icon.svg');
+//   });
+// });
