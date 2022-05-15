a6abfa9720c8c46e407ea65cfa4e7e4f07c22ebb
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-05-14 22:00:11 -0300

    fix req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -5,17 +5,18 @@ import RenderWithRouter from './RenderWithRouter';
 
 describe('Testa se <FavoritePokemons.js /> tem o comportamento sperado', () => {
   it('Testa se a página exibe o conteudo correto', () => {
-    const { getByText, getByRole,
+    const { getByText, getByRole, getAllByRole,
     } = RenderWithRouter(<App />);
 
     const favoritePage = getByRole('link', { name: /Favorite Pokémons/i });
+    const homePage = getByRole('link', { name: /home/i });
 
     userEvent.click(favoritePage);
 
     const favoriteNotFound = getByText(/No favorite pokemon found/i);
     expect(favoriteNotFound).toBeInTheDocument();
 
-    userEvent.click(getByRole('link', { name: /home/i }));
+    userEvent.click(homePage);
     userEvent.click(getByRole('link', { name: /More details/i }));
 
     userEvent.click(getByRole('checkbox', { name: /Pokémon favoritado/i }));
@@ -23,5 +24,14 @@ describe('Testa se <FavoritePokemons.js /> tem o comportamento sperado', () => {
 
     expect(favoriteNotFound).not.toBeInTheDocument();
     expect(getByText(/Pikachu/i)).toBeInTheDocument();
+
+    userEvent.click(homePage);
+    userEvent.click(getByRole('button', { name: /Próximo pokémon/i }));
+    userEvent.click(getByRole('link', { name: /More details/i }));
+    userEvent.click(getByRole('checkbox', { name: /Pokémon favoritado/i }));
+    userEvent.click(favoritePage);
+
+    expect(favoriteNotFound).not.toBeInTheDocument();
+    expect(getAllByRole('link', { name: /More details/i })).toHaveLength(2);
   });
 });
